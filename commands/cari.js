009d9eb381c237147b7e67886fd966ddb16727eb
chariselyasa123 <ahmad.charis@raharja.info>
2020-12-18 23:30:38 +0700

    nambah perintah pencarian

---
+++ commands/cari.js
@@ -0,0 +1,31 @@
+const search = require('yt-search');
+
+exports.run = async (client, msg, args, ops) => {
+
+    search(args.join(' '), (err, res) => {
+        if (err) {
+            console.log(err);
+            return msg.channel.send('❗ Maaf terjadi kesalahan.');
+        }
+
+        let videos = res.videos.slice(0, 10);
+
+        let resp = '';
+        for (var i in videos) {
+            resp += `**[${parseInt(i)+1}]** \`${videos[i].title}\`\n`;
+        }
+
+        resp += `\n**Pilih nomer antara \`1-${videos.length}\``;
+
+        msg.channel.send(resp);
+
+        const filter = m => !isNaN(m.content) && m.content < videos.length + 1 && m.content > 0;
+        const colektor = msg.channel.createMessageCollector(filter);
+
+        colektor.videos = videos;
+        colektor.on('collect', m => {
+            let commandFile = require(`./musik`);
+            commandFile.run(client, msg, [videos[parseInt(m.content) - 1].url], ops);
+        });
+    });
+}
