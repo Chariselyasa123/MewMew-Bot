8ac0693507d80bc43d61874c6285f86d36e598de
chariselyasa123 <ahmad.charis@raharja.info>
2020-12-19 09:01:39 +0700

    lupa return awoakwoawkok

--- commands/musik.js
+++ commands/musik.js
@@ -11,7 +11,7 @@ exports.run = async (client, msg, args, ops) => {
 
     if (!validasi) {
         let commandFile = require('./cari');
-        commandFile.run(client, msg, args, ops);
+        return commandFile.run(client, msg, args, ops);
     }
 
     // if (!validasi) return msg.channel.send('❗ Kasih link yang bener Woi!! 😠');
