f5aa1206930499ef266149c8549edb38298c022f
chariselyasa123 <ahmad.charis@raharja.info>
2020-12-20 00:04:01 +0700

    nambah perintah

--- commands/perintah.js
+++ commands/perintah.js
@@ -6,11 +6,11 @@ exports.run = (client, msg, args, ops, embed) => {
         .setColor('#40eb34')
         .addFields({
             name: 'Perintah',
-            value: '``+musik``\n\n``+cari``\n\n``+antrian``\n\n``+jeda``\n\n``+lanjut``\n\n``+ganti``\n\n``+vol``\n\n``+ping``\n\n``+crot``',
+            value: '``+musik``\n\n``+cari``\n\n``+antrian``\n\n``+jeda``\n\n``+lanjut``\n\n``+ganti``\n\n``+vol``\n\n``+ping``\n\n``+crot``\n\n``+user-info``',
             inline: true
         }, {
             name: 'Deskripsi',
-            value: '``+musik`` [Link Youtube] atau ``+musik`` [Judul Lagu]\n\n``+cari`` [Judul Lagu] lalu pilih 1-10\n\nUntuk Melihat lagu yang ada di antrian (playlist)\n\nUntuk menjeda (pause) lagu\n\nUntuk melanjutkan lagu yang dijeda (pause)\n\nUntuk mengganti lagu ke antrian berikutnya\n\nMenambah atau mengurangi volume pilih no 0 - 200\n\nCek Latensi MewMew Bot 💩\n\nMengeluarkan bot dari voice channel',
+            value: '``+musik`` [Link Youtube] atau ``+musik`` [Judul Lagu]\n\n``+cari`` [Judul Lagu] lalu pilih 1-10\n\nUntuk Melihat lagu yang ada di antrian (playlist)\n\nUntuk menjeda (pause) lagu\n\nUntuk melanjutkan lagu yang dijeda (pause)\n\nUntuk mengganti lagu ke antrian berikutnya\n\nMenambah atau mengurangi volume pilih no 0 - 200\n\nCek Latensi MewMew Bot 💩\n\nMengeluarkan bot dari voice channel\n\n``+user-info`` [mention orang di server ini] atau hanya ``+user-info`` saja untuk mengetahui info id discord klean',
             inline: true
         })
     msg.channel.send(sms);
