ba2f236763f237edf8cec4ab496a95bba828033b
chariselyasa123 <ahmad.charis@raharja.info>
2020-12-19 11:18:09 +0700

    Destroy

--- commands/crot.js
+++ commands/crot.js
@@ -1,13 +1,18 @@
 exports.run = (client, msg, args, ops) => {
 
+    let ambil = ops.active.get(msg.guild.id);
+
+    if (!ambil) return msg.channel.send('❗ Lagi ga ada musik yang lagi diputar!');
+
     if (!msg.member.voice.channel) return msg.channel.send('❗ Masuk ke voice channel dulu KK!')
 
     if (!msg.guild.me.voice.channel) return msg.channel.send('❗ Maaf MewMew Bot 💩 lagi ga di dalem voice channel nih');
 
     if (msg.member.voice.channel !== msg.guild.me.voice.channel) return msg.channel.send('❗ Maaf kamu sekarang berada di channel yang berbeda dengan MewMew Bot 💩');
 
+    ambil.dispatcher.destroy();
     msg.guild.me.voice.channel.leave();
 
-    msg.channel.send('Keluar Channel....')
+    msg.channel.send('Keluar Channel....');
 
 }
